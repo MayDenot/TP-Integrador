720725696200dbba85a99de50e9b985b0808cd71
Valentin <valenfacu316@gmail.com>
2025-09-29 16:05:22 -0300

    implementomos metodos en DaoImplEstudiante

--- parte2/src/main/java/repository/DaoImplEstudiante.java
+++ parte2/src/main/java/repository/DaoImplEstudiante.java
@@ -2,6 +2,7 @@ package repository;
 
 import entites.Estudiante;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
 
 import java.util.List;
 
@@ -20,21 +21,39 @@ public class DaoImplEstudiante implements DaoEstudiante {
 
     @Override
     public List<Estudiante> getEstudiantes() {
-        return List.of();
+        return em.createQuery("SELECT e FROM Estudiante e", Estudiante.class)
+                .getResultList();
     }
 
-    @Override
     public Estudiante getEstudianteByLibreta(int libreta) {
-        return null;
+        try {
+            return em.createQuery(
+                            "SELECT e FROM Estudiante e WHERE e.nroLU = :libreta",
+                            Estudiante.class)
+                    .setParameter("libreta", libreta)
+                    .getSingleResult();
+        } catch (NoResultException e) {
+            throw new RuntimeException("No existe estudiante con libreta " + libreta);
+        }
     }
 
     @Override
-    public List<Estudiante> getEstudiantesByGenero(String genero) {
-        return List.of();
+    public List getEstudiantesByGenero(String genero) {
+        try{
+            return  em.createQuery("SELECT e FROM Estudiante e where e.genero = :genero ").setParameter("genero", genero).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("No existe estudiante con este genero " + genero);
+
+        }
     }
 
     @Override
-    public List<Estudiante> getEstudiantesByCarreraYCiudad(int id_carrera, String ciudad) {
-        return List.of();
+    public List getEstudiantesByCarreraYCiudad(int idCarrera, String ciudad) {
+        try {
+            return em.createQuery("SELECT ec.estudiante FROM Estudiante_Carrera  ec where ec.carrera.idCarrera=:idCarrera AND ec.estudiante.ciudad=:ciudad")
+                    .setParameter(idCarrera, idCarrera).setParameter("ciudad", ciudad).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("No hay estudiantes con esas " + idCarrera + " " + ciudad);
+        }
     }
 }
